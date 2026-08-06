2d7b6bb04ecf3b7c9df9fee9ac862ea7a3e9e676
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-08-06 13:28:51 +0530

    uptimization

---
+++ app/profile/page.tsx
@@ -0,0 +1,9 @@
+export default function ProfilePage() {
+  return (
+    <div className="min-h-screen w-full dark:bg-[radial-gradient(125%_125%_at_50%_10%,#000000_40%,#010133_100%)]">
+      <div className="absolute">
+        <h1 className="text-2xl font-bold">Profile Page</h1>
+      </div>
+    </div>
+  );
+}
