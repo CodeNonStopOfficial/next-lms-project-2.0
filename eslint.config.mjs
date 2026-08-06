224a028fef467c1afbc807ce5f2117d4b40b508e
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-08-07 01:58:28 +0530

    fix some bug

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -18,8 +18,6 @@ const eslintConfig = defineConfig([
       "react-hooks/set-state-in-effect": "off",
       "@typescript-eslint/no-explicit-any": "off",
        "react-hooks/incompatible-library": "off",
-      //  'react/no-unescaped-entities': 'off',
-      // '@next/next/no-page-custom-font': 'off',
     },
   },
 ]);
