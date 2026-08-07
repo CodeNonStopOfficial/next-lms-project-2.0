7e70ffd8abad839a6f4804fef18267a120f127da
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-08-07 11:41:00 +0530

    security arject

--- app/api/s3/delete/route.ts
+++ app/api/s3/delete/route.ts
@@ -2,9 +2,37 @@ import { env } from "@/lib/env";
 import { S3 } from "@/lib/S3Client";
 import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { NextResponse } from "next/server";
+import arject, { detectBot, fixedWindow } from "@/lib/arject";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
+
+
+const aj = arject.withRule(
+   detectBot({
+      mode :"LIVE",
+      allow : [],
+   })
+).withRule(
+   fixedWindow({
+       mode : "LIVE",
+       window :"1m",
+       max : 5
+   })
+)
 
 export async function DELETE(request:Request){
+   const session = await auth.api.getSession({
+       headers : await headers()
+   })
    try {
+      const desision = await aj.protect(request,{
+         fingerprint : session?.user.id as string
+      })
+      if(desision.isDenied()){
+          return NextResponse.json({
+             error:"Upload Bot Delete Bad"
+          },{status : 429})
+      }
       const body = await request.json();
       const key = body.key;
       if(!key){
--- app/api/s3/upload/route.ts
+++ app/api/s3/upload/route.ts
@@ -5,10 +5,32 @@ import {v4 as uuidv4} from "uuid";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { S3 } from "@/lib/S3Client";
 import { fileUploadeSchema } from "./schema";
+import arject, { detectBot, fixedWindow } from "@/lib/arject";
+import { auth } from "@/lib/auth";
+import { headers } from "next/headers";
 
+const aj = arject.withRule(
+   detectBot({
+      mode :"LIVE",
+      allow : [],
+   })
+).withRule(
+   fixedWindow({
+       mode : "LIVE",
+       window :"1m",
+       max : 5
+   })
+)
 
 export async function POST(request : Request){
+   const session = await auth.api.getSession({
+       headers : await headers()
+   })
    try{
+      const decision = await aj.protect(request,{fingerprint:session?.user.id as string});
+      if(decision.isDenied()){
+          return NextResponse.json({error:"Upload Bot Not Well"},{status:429})
+      }
       const body = await request.json();
       const validation = fileUploadeSchema.safeParse(body);
       if(!validation.success){
