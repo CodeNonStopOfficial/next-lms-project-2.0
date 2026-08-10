5c290372bdbfa7de50cca22780efbefc4363b9a5
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-08-11 01:16:27 +0530

    add lecture dnd.kit drop ocmon

--- app/admin/course/[courseId]/edit/_components/CourseStucture.tsx
+++ app/admin/course/[courseId]/edit/_components/CourseStucture.tsx
@@ -1,7 +1,81 @@
-export function CourseStucture(){
-     return (
-         <div>
-             <h1>Course Stucture</h1>
-         </div>
-     )
+"use client";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import {
+  DndContext,
+  KeyboardSensor,
+  PointerSensor,
+  rectIntersection,
+  useSensor,
+  useSensors,
+} from "@dnd-kit/core";
+import {
+  arrayMove,
+  SortableContext,
+  sortableKeyboardCoordinates,
+  verticalListSortingStrategy,
+} from "@dnd-kit/sortable";
+import { useState } from "react";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
+
+export function CourseStucture() {
+  const [item, setItems] = useState(["1", "2", "3"]);
+
+  const sensors = useSensors(
+    useSensor(PointerSensor),
+    useSensor(KeyboardSensor, {
+      coordinateGetter: sortableKeyboardCoordinates,
+    }),
+  );
+
+
+  function handleDragEnd(event: any) {
+    const { active, over } = event;
+
+    if (active.id !== over.id) {
+      setItems((items) => {
+        const oldIndex = items.indexOf(active.id);
+        const newIndex = items.indexOf(over.id);
+
+        return arrayMove(items, oldIndex, newIndex);
+      });
+    }
+  }
+  return (
+    <DndContext
+      sensors={sensors}
+      collisionDetection={rectIntersection}
+      onDragEnd={handleDragEnd}
+    >
+      <Card>
+        <CardHeader className="flex flex-row items-center justify-between border-b border-border">
+          <CardTitle>Chapter-1</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <SortableContext strategy={verticalListSortingStrategy} items={item}>
+            {item.map((id) => (
+              <SortableItem key={id} id={id} />
+            ))}
+          </SortableContext>
+        </CardContent>
+      </Card>
+    </DndContext>
+  );
 }
+
+
+function SortableItem(props: any) {
+    const { attributes, listeners, setNodeRef, transform, transition } =
+      useSortable({ id: props.id });
+
+    const style = {
+      transform: CSS.Transform.toString(transform),
+      transition,
+    };
+
+    return (
+      <div ref={setNodeRef} style={style} {...attributes} {...listeners}>
+        {props.id}
+      </div>
+    );
+  }
