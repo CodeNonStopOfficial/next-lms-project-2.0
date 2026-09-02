b8ae305ef82fb7acf8d7a0d2b960fdec15f9841f
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-09-03 00:47:51 +0530

    fix

--- app/(shared-layout)/course/[slug]/actions.ts
+++ app/(shared-layout)/course/[slug]/actions.ts
@@ -131,7 +131,7 @@ export async function enrollInCourseAction(
         customer : stripeCustomerId,
         line_items: [
           {
-            price: "price_1UBEeOJAuShPj15nnfh8qLkR",
+            price:"price_1UBJeGH0duXAAuN0zElDFZti"
             quantity: 1,
           },
         ],
