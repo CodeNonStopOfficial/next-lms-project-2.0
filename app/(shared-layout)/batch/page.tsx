46232dfd8990c447e6d61fe69c23ae57c3dbf025
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-09-08 05:38:22 +0530

    add

---
+++ app/(shared-layout)/batch/page.tsx
@@ -0,0 +1,7 @@
+export default function BatchPage(){
+     return (
+         <div className="flex items-center justify-center mx-auto mt-51">
+             <h1 className="text-2xl items-center justify-center">In Future</h1>
+         </div>
+     )
+}
