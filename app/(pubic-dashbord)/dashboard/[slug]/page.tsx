a648a4cb85ae87faacc4428f8dfe29c0b0f7faab
BCA-445-11397-Virendra <166232903+CodeNonStopOfficial@users.noreply.github.com>
2026-09-09 01:15:18 +0530

    v1 success

--- app/(pubic-dashbord)/dashboard/[slug]/page.tsx
+++ app/(pubic-dashbord)/dashboard/[slug]/page.tsx
@@ -8,8 +8,8 @@ interface iAppProps {
 export default async function CourseSlugPage({params}:iAppProps) {
   const {slug} = await params;
   const {course} = await getCourseSidebarData(slug);
-  const firstChapter = course.chapter[0];
-  const firstLesson = firstChapter.lessons[0];
+  const firstChapter = course?.chapter?.[0];
+  const firstLesson = firstChapter?.lessons?.[0];
   if(firstLesson){
      redirect(`/dashboard/${slug}/${firstLesson.id}`);
   }
